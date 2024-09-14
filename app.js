14448eef7420d8dddb03fb59efa356de1249c681
krishna shekhawat <kishushekhawat1@gmail.com>
2024-09-14 20:44:42 +0530

    hideDiv added

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let navbar = document.querySelector('#navbar');
+let navend= document.querySelector('#navend');
+
 
 
 
@@ -6,12 +8,23 @@ let navbar = document.querySelector('#navbar');
 
 
 window.addEventListener('scroll',()=>{
-    console.log(window.scrollY);
+  
     if(window.scrollY > 500){
         navbar.classList.add('show')
+     
      }
     else{
         navbar.classList.remove('show')
      }
+     hideDiv();
+  
 })
 
+function hideDiv() {
+    navend.classList.add('hidden');
+    
+    setTimeout(() => {
+        navend.classList.remove('hidden');
+    }, 4000);
+
+}
