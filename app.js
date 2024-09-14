1df184c60482fadf07002e4eba2a3b2d4736be52
krishna shekhawat <kishushekhawat1@gmail.com>
2024-09-14 14:22:38 +0530

    postion fixed

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ let navbar = document.querySelector('#navbar');
 
 
 window.addEventListener('scroll',()=>{
-    if(window.scrollY > 1500){
-        navbar.classList.remove('show')
+    console.log(window.scrollY);
+    if(window.scrollY > 500){
+        navbar.classList.add('show')
      }
     else{
-        navbar.classList.add('show')
+        navbar.classList.remove('show')
      }
 })
 
